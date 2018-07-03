5a88d04265b185aa7e2faa5321c41c00d184e45a
mohammad hossain <you@example.com>
2018-07-03 17:00:19 +0430

    some times dead lock occured i dont know the reason

--- BarberShop.java
+++ BarberShop.java
@@ -11,27 +11,26 @@ public class BarberShop {
     static Semaphore coord = new Semaphore(3);
     static Semaphore mutex1 = new Semaphore(1);
     static Semaphore mutex2 = new Semaphore(1);
-    static Semaphore cust_ready = new Semaphore(0);
-    static Semaphore leave_barber_chair = new Semaphore(0);
-    static Semaphore payment = new Semaphore(0);
-    static Semaphore receipt = new Semaphore(0);
+    static Semaphore cust_ready = new Semaphore(1);
+    static Semaphore leave_barber_chair = new Semaphore(1);
+    static Semaphore payment = new Semaphore(1);
+    static Semaphore receipt = new Semaphore(1);
     static Semaphore finished[] = new Semaphore[50];
     static Queue<Integer> queue=new LinkedList<>();
-    static int count;
+    static int count=0;
 
 
-    class Customer extends Thread{
-        private int number;
+    static class Customer extends Thread{
+        private int custnr;
 
-        public int getNumber() {
-            return number;
+        public int getCustomerNumber() {
+            return custnr;
         }
 
-        public Customer() throws InterruptedException {
-            mutex1.acquire();
-            this.number = count;
-            count++;
-            mutex1.release();
+        public Customer()  {
+            for(int i=0;i<finished.length;i++){
+                finished[i]=new Semaphore(1);
+            }
         }
 
         @Override
@@ -39,31 +38,90 @@ public class BarberShop {
 
             try {
                 max_capacity.acquire();
-                delay();// eneter shop
-                System.out.println(getNumber()+"entered shop");
+                delay();
+                System.out.println("new customer comes in");
+                mutex1.acquire();
+                custnr=count;
+                count++;
+                mutex1.release();
                 sofa.acquire();
-                delay();//sit on sofa
+                delay(); // customers sits on sofa
                 barber_chair.acquire();
-                delay();//get up from sofa
+                delay();//customer gets up from sofa
                 sofa.release();
-                delay();//sit in barber chair
+                sit_on_barber_chair();
                 mutex2.acquire();
-                queue.add(getNumber());
                 cust_ready.release();
-                mutex2.acquire();
-                finished[getNumber()].acquire();
-                delay();//leave barber chair
+                mutex2.release();
+                System.out.println(String.format("customer number is %d", getCustomerNumber()));
+                finished[custnr].acquire();
+                leave_barber_chair();
                 leave_barber_chair.release();
-                delay();//pay
+                pay();
                 payment.release();
                 receipt.acquire();
-                delay();//exit from shop
-                max_capacity.release();
-
+                exit_shop();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
+        }
+
+        private void pay() {
+            cashier();
+            delay();
+            System.out.println(String.format("customer %d payed his payment", getCustomerNumber()));
+
+        }
+        private void cashier(){
+            while (true){
+                try {
+                    payment.acquire();
+                    coord.acquire();
+                    System.out.println(String.format("payment from %d accepted" , getCustomerNumber() ));
+                    coord.release();
+                    receipt.release();
+                    return;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void exit_shop() {
+            receipt.release();
+            System.out.println(String.format("customer %d has left the shop",getCustomerNumber() ));
+        }
+
+
+        private void leave_barber_chair() {
+                leave_barber_chair.release();
+        }
+
+        private void sit_on_barber_chair() {
+            queue.add(getCustomerNumber());
+            barber();
+            System.out.println(String.format("customer %d sits on barber chair.",getCustomerNumber()));
+            delay();
+
+        }
+
+        private void barber(){
+            int b_cust;
+                try {
+                    cust_ready.acquire();
+                    mutex2.acquire();
+                    b_cust=queue.remove();
+                    mutex2.release();
+                    coord.acquire();
+                    cut_hair(b_cust);
+                    coord.release();
+                    finished[b_cust].release();
+                    leave_barber_chair.acquire();
+                    barber_chair.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
         }
 
         public void delay(){
@@ -74,16 +132,42 @@ public class BarberShop {
             }
         }
 
+        public void cut_hair(int b_cust){
+            try {
+                Thread.sleep(2000);
+                finished[b_cust].acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
+
+
+
+
+
 
 
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        count=0;
+//        while(true){
+//            i=input.nextInt();
+//            if (i==0) {
+//                    Customer c= new Customer();
+//                    c.start();
+//            }
+//        }
+        for(int i=0;i<100;i++){
+            Customer c= new Customer();
+            c.start();
+        }
+        //count=0;
         // initialize finished semaphor
-        for (int i=0;i<50;i++)
-            finished[i]=new Semaphore(0);
+//        for (int i=0;i<50;i++)
+//            finished[i]=new Semaphore(0);
+
 
     }
 }
---
+++ out/production/barbershop/README.md
@@ -0,0 +1,8 @@
+# Simple implementation of barbershop problem with java
+
+# Todolist
+
+- [x] write customer class
+- [ ] write barber class
+- [ ] write cashier class
+- [ ] complete main method for final test
